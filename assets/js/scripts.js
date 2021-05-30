a78b96a71a8f5af6e38a36686fe0f286f4089829
Bradley Reeves <reevesbra@outlook.com>
2021-05-29 21:09:48 -0700

    testing

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -161,12 +161,14 @@
     const value = 'application/x-www-form-urlencoded';
 
     const postData = (event) => {
-        //console.log(event.srcElement.attributes[1].value);
         var myString = String(event.srcElement.attributes[1].value);
-        var sentence_id = myString.charAt(myString.length - 1);
+        var sentence_id = myString.slice[-1];
 
+        console.log("type:")
         console.log(typeof myString)
+        console.log("string")
         console.log(myString)
+        console.log("id")
         console.log(sentence_id)
 
         // Create request object
